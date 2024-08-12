3bf63313daf5c1139b8c1c7b1bb4204c90f4972d
imranpopz <imranpopz32@gmail.com>
2024-08-12 04:58:34 +0000

    Revert "Settings: Allow locking all apps visible in launcher"
    This reverts commit 6b8f8e00cd5c49652ca5f20ad17a1f0fd8979588.

--- src/com/android/settings/security/applock/AppLockPackageListFragment.kt
+++ src/com/android/settings/security/applock/AppLockPackageListFragment.kt
@@ -29,7 +29,6 @@ import androidx.preference.Preference
 import androidx.preference.forEach
 
 import com.android.internal.logging.nano.MetricsProto
-import com.android.internal.util.aospaplus.Utils
 
 import com.android.settings.R
 import com.android.settings.core.SubSettingLauncher
@@ -49,13 +48,11 @@ class AppLockPackageListFragment : DashboardFragment() {
     private lateinit var appLockManager: AppLockManager
     private lateinit var pm: PackageManager
     private lateinit var whiteListedPackages: Array<String>
-    private lateinit var launchablePackages: List<String>
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         appLockManager = context.getSystemService(AppLockManager::class.java)
         pm = context.packageManager
-        launchablePackages = Utils.launchablePackages(context)
         whiteListedPackages = resources.getStringArray(
             com.android.internal.R.array.config_appLockAllowedSystemApps)
     }
@@ -68,9 +65,7 @@ class AppLockPackageListFragment : DashboardFragment() {
                 pm.getInstalledPackages(
                     PackageInfoFlags.of(PackageManager.MATCH_ALL.toLong())
                 ).filter {
-                    !it.applicationInfo.isSystemApp() ||
-                        launchablePackages.contains(it.packageName) ||
-                        whiteListedPackages.contains(it.packageName)
+                    !it.applicationInfo.isSystemApp() || whiteListedPackages.contains(it.packageName)
                 }.sortedWith { first, second ->
                     getLabel(first).compareTo(getLabel(second))
                 }
